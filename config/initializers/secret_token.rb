392cd91417780016509e548f7d7ffdc971ba30ce
Laura Khalil <zazenergy@gmail.com>
2012-08-23 16:43:47 -0700

    Initial commit. Let's start breaking stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetProject::Application.config.secret_token = '074ae28bb50c0a900d9b574df09f18e493f680aeff4865b5f255caed17f8ee47b62b726773253006d75364c9d247fd8606bd0c3e239958dfa2a8d682ab2872a6'
