b68e745f9e945d75a5a8827c8a378d44925bbe55
Laura Khalil <zazenergy@gmail.com>
2012-09-04 20:43:06 -0700

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,10 @@
 
 class User < ActiveRecord::Base
 #Ensures that ONLY name and email attributes are accessible to outside users.
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
-  
+  has_many :microposts, dependent: :destroy
+    
   before_save { |user| user.email = email.downcase}
   before_save :create_remember_token
   
@@ -24,6 +25,11 @@ class User < ActiveRecord::Base
   							uniqueness: { case_sensitive: false}
   validates :password, presence: true, length: {minimum: 6}
   validates :password_confirmation, presence: true
+  
+  def feed
+     # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
   
   private 
   	
